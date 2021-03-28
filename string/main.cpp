5eddf2867c10b300cc988284756557283d45f99d
GP-R <48301097+GP-R@users.noreply.github.com>
2021-03-28 14:31:48 +0900

    class화

--- string/main.cpp
+++ string/main.cpp
@@ -2,36 +2,50 @@
 #include<stdio.h>
 #include<string.h>
 
-char* removeLetterFromString(char data[], int len, char letter)
-{
-	for (; *data != 0; data++) // hello world ello world llo world.....
+class MyString {
+	char* data;
+	int len;
+public:
+	MyString(char* str, int len)
+		:len(len)
 	{
-		if (*data == letter) // 'l' == 'l'
+		data = new char;
+		strcpy(data, str);
+	}
+	~MyString()
+	{
+
+	}
+	char* removeLetterFromString(char letter)
+	{
+		for (; *data != 0; data++) // hello world ello world llo world.....
 		{
-			strcpy(data, data + 1); // hello world -> helo world // llo world lo world -> lo world // 덮어씌우기
-			data--; //elo world
-			len--;
+			if (*data == letter) // 'l' == 'l'
+			{
+				strcpy(data, data + 1); // hello world -> helo world // llo world lo world -> lo world // 덮어씌우기
+				data--; //elo world
+				len--;
+			}
 		}
+		data -= len; // data를 플러스했으므로 원래대로 되돌리려면 글자수만큼 빼줘야함
+		return data;
 	}
-	data -= len; // data를 플러스했으므로 원래대로 되돌리려면 글자수만큼 빼줘야함
-	return data;
-}
-
-char* replaceLetterFromString(char data[], int len, char letter)
-{
-	for (int i = 0; i < len; i++)
+	char* replaceLetterFromString(char letter)
 	{
-		if (data[i] == letter)
-			data[i] = 0;
+		for (int i = 0; i < len; i++)
+		{
+			if (data[i] == letter)
+				data[i] = letter;
+		}
+		return data;
 	}
-	return data;
-}
 
+};
 void main()
 {
-	char data[16] = "Hello World";
-	int len = strlen(data);
-	char* result = removeLetterFromString(data, len, 'l');
+	char str[] = "Hello World";
+	MyString mystr(str, strlen(str));
+	char* result = mystr.removeLetterFromString('l');
 	printf("%s", result);
 	return;
 }
